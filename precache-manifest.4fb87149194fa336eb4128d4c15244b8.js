cae0aed0ada7af9b1cde6576231e785664436cbf
sarafotros <sarafotros@gmail.com>
2020-07-28 17:29:45 +0100

    Updates

---
+++ precache-manifest.4fb87149194fa336eb4128d4c15244b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4cb4c8888107b68b50a6cf702f6321a",
+    "url": "/react-callme/index.html"
+  },
+  {
+    "revision": "77c3910384d395783fa4",
+    "url": "/react-callme/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91421454993f05c1d00f",
+    "url": "/react-callme/static/js/2.7b12d0e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-callme/static/js/2.7b12d0e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c3910384d395783fa4",
+    "url": "/react-callme/static/js/main.e2916552.chunk.js"
+  },
+  {
+    "revision": "af34c3efda3bfdd964a9",
+    "url": "/react-callme/static/js/runtime-main.a1a3b4e3.js"
+  }
+]);
