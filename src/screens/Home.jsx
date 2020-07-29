b1762fa772a437e5af857ed7530fe305216f826d
sarafotros <sarafotros@gmail.com>
2020-07-29 14:41:56 +0100

    update function refactor home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,108 +3,135 @@ import styles from './styles';
 import SignInForm from './SignInForm';
 
 export default class Home extends Component {
-  state = {
-    list: [],
-  };
-  handleSignIn = (newEntry) => {
-    this.setState((prevState) => ({ list: [...prevState.list, newEntry] }));
-  };
-  handleDelete = (allList) => {
-    const filtered = this.state.list.filter((ls) => ls !== allList);
-    this.setState({
-      list: filtered,
-    });
-  };
-  handleEdit = (input) => {
-    console.log(input);
-  };
-  render() {
-    return (
-      <div style={styles.body}>
-        <div style={styles.centerTitle}>
-          <h1>Address Book</h1>
-        </div>
-        <div style={styles.main}>
-          <SignInForm handleSignIn={this.handleSignIn} />
-        </div>
-        <div>contacts:</div>
+	initialItem = {
+		username: '',
+		email: '',
+		phone: '',
+	};
+	state = {
+		item: { ...this.initialItem },
+		list: [],
+		editMode: false,
+	};
+	handleSignIn = () => {
+		this.setState((prevState) => ({
+			list: [...prevState.list, prevState.item],
+			item: { username: '', email: '', phone: '' },
+		}));
+	};
+	handleDelete = (allList) => {
+		const filtered = this.state.list.filter((ls) => ls !== allList);
+		this.setState({
+			list: filtered,
+		});
+	};
+	handleEdit = (input) => {
+		console.log(input);
+		this.setState({
+			item: input,
+			editMode: true,
+		});
+	};
 
-        {this.state.list.length > 0 ? (
-          <table style={styles.table}>
-            <thead>
-              <tr style={styles.tableTr}>
-                <th style={thStyle}>USERNAME</th>
-                <th style={thStyle}>EMAIL ADDRESS</th>
-                <th style={thStyle}>PHONE NUMBER</th>
-                <th style={thStyleDel}></th>
-              </tr>
-            </thead>
-            {this.state.list.map((item) => (
-              <tbody>
-                <tr key={item.username} style={styles.tableTr}>
-                  <td style={tdStyle}>{item.username}</td>
-                  <td style={tdStyle}>{item.email}</td>
-                  <td style={tdStyle}>{item.phone}</td>
-                  <td style={tdStyle}>
-                    <button
-                      onClick={() => this.handleEdit(item)}
-                      style={btnEdit}
-                    >
-                      EDIT
-                    </button>
-                    <button
-                      onClick={() => this.handleDelete(item)}
-                      style={btnDel}
-                    >
-                      DELETE
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            ))}
-          </table>
-        ) : null}
-      </div>
-    );
-  }
+	changeItemsProps = ({ nam, val }) => {
+		let newItem = { ...this.state.item, [nam]: val };
+		console.log(newItem);
+		this.setState({
+			item: newItem,
+		});
+	};
+	render() {
+		return (
+			<div style={styles.body}>
+				<div style={styles.centerTitle}>
+					<h1>Address Book</h1>
+				</div>
+				<div style={styles.main}>
+					<SignInForm
+						handleSignIn={this.handleSignIn}
+						item={{ ...this.state.item }}
+						changeItemsProps={this.changeItemsProps}
+						editMode={this.state.editMode}
+					/>
+				</div>
+				<div>contacts:</div>
+
+				{this.state.list.length > 0 ? (
+					<table style={styles.table}>
+						<thead>
+							<tr style={styles.tableTr}>
+								<th style={thStyle}>USERNAME</th>
+								<th style={thStyle}>EMAIL ADDRESS</th>
+								<th style={thStyle}>PHONE NUMBER</th>
+								<th style={thStyleDel}></th>
+							</tr>
+						</thead>
+						{this.state.list.map((item, index) => (
+							<tbody>
+								<tr key={item.index} style={styles.tableTr}>
+									<td style={tdStyle}>{item.username}</td>
+									<td style={tdStyle}>{item.email}</td>
+									<td style={tdStyle}>{item.phone}</td>
+									<td style={tdStyle}>
+										<button
+											onClick={() => this.handleEdit(item, index)}
+											style={btnEdit}
+										>
+											EDIT
+										</button>
+										<button
+											onClick={() => this.handleDelete(item)}
+											style={btnDel}
+										>
+											DELETE
+										</button>
+									</td>
+								</tr>
+							</tbody>
+						))}
+					</table>
+				) : null}
+			</div>
+		);
+	}
 }
 const thStyle = {
-  width: '27%',
-  border: 'none',
-  height: 30,
-  fontFamily: '"Special Elite", cursive',
-  borderBottom: '1px solid #000',
-  backgroundColor: 'white',
+	width: '27%',
+	border: 'none',
+	height: 30,
+	fontFamily: '"Special Elite", cursive',
+	borderBottom: '1px solid #000',
+	backgroundColor: 'white',
 };
 const thStyleDel = {
-  width: '19%',
-  border: 'none',
-  height: 30,
-  fontFamily: '"Special Elite", cursive',
-  borderBottom: '1px solid #000',
-  backgroundColor: 'white',
+	width: '19%',
+	border: 'none',
+	height: 30,
+	fontFamily: '"Special Elite", cursive',
+	borderBottom: '1px solid #000',
+	backgroundColor: 'white',
 };
 const tdStyle = {
-  border: 'none',
-  height: 40,
-  borderBottom: '1px solid #000',
-  backgroundColor: '#bdbdbd',
+	border: 'none',
+	height: 40,
+	borderBottom: '1px solid #000',
+	backgroundColor: '#bdbdbd',
 };
 const btnDel = {
-  padding: 8,
-  width: '40%',
-  fontFamily: '"Special Elite", cursive',
-  backgroundColor: '#8d8d8d',
-  border: 'none',
-  borderRadius: 4,
-  marginLeft: 6,
+	padding: 8,
+	width: '40%',
+	fontFamily: '"Special Elite", cursive',
+	backgroundColor: '#8d8d8d',
+	border: 'none',
+	borderRadius: 4,
+	marginLeft: 6,
 };
 const btnEdit = {
-  padding: 8,
-  width: '40%',
-  fontFamily: '"Special Elite", cursive',
-  backgroundColor: '#E0E0E0',
-  border: 'none',
-  borderRadius: 4,
-  marginRight: 6,
+	padding: 8,
+	width: '40%',
+	fontFamily: '"Special Elite", cursive',
+	backgroundColor: '#E0E0E0',
+	border: 'none',
+	borderRadius: 4,
+	marginRight: 6,
 };
--- src/screens/SignInForm.jsx
+++ src/screens/SignInForm.jsx
@@ -4,79 +4,83 @@ import InputBox from '../components/InputBox';
 import Btn from '../components/Btn';
 
 export default class SignInForm extends Component {
-  state = {
-    username: '',
-    email: '',
-    phone: '',
-    showMsg: false,
-  };
+	state = {
+		username: '',
+		email: '',
+		phone: '',
+		showMsg: false,
+	};
 
-  onChangeHandle = (e) => {
-    let nam = e.target.name;
-    let val = e.target.value;
-    this.setState({ [nam]: val });
-  };
-  //   handleSignIn = (newEntry) => {
-  //     this.setState((prevState) => ({ list: [...prevState.list, newEntry] }));
-  //   };
-  handleSubmitForm = (e) => {
-    e.preventDefault();
-    let { username, email, phone } = this.state;
-    this.props.handleSignIn({ username, email, phone });
-    this.setState({ username: '', email: '', phone: '' });
-  };
+	onChangeHandle = (e) => {
+		let nam = e.target.name;
+		let val = e.target.value;
+		this.props.changeItemsProps({ nam, val });
+	};
+	//   handleSignIn = (newEntry) => {
+	//     this.setState((prevState) => ({ list: [...prevState.list, newEntry] }));
+	//   };
+	handleSubmitForm = (e) => {
+		e.preventDefault();
+		this.props.handleSignIn();
+	};
 
-  myBtnClick = () => {
-    this.setState({
-      username: this.state.username,
-      showMsg: true,
-    });
-  };
+	myBtnClick = () => {
+		this.setState({
+			username: this.state.username,
+			showMsg: true,
+		});
+	};
 
-  render() {
-    let isDisabled =
-      this.state.username === '' ||
-      this.state.email === '' ||
-      this.state.phone === '';
-    return (
-      <form style={styles.form} onSubmit={this.handleSubmitForm}>
-        <h2 style={{ textAlign: 'center' }}>New Contact</h2>
-        <InputBox
-          nameProp='username'
-          placeholder='Username'
-          regex={/^[a-z0-9]{2,}$/}
-          errorText='username cannot include space'
-          value={this.state.username}
-          onChangeProps={this.onChangeHandle}
-        />
-        <InputBox
-          nameProp='email'
-          placeholder='Email'
-          regex={/^[a-z0-9]{3,}@[2-z]{2,}\.[a-z]{2,4}$/i}
-          errorText='invalid E-mail format'
-          value={this.state.email}
-          onChangeProps={this.onChangeHandle}
-        />
-        <InputBox
-          nameProp='phone'
-          placeholder='Phone No.'
-          regex={/^07[0-9]{9}$/}
-          errorText='invalid Mobile Number format'
-          value={this.state.phone}
-          onChangeProps={this.onChangeHandle}
-        />
-        <div style={styleBtn}>
-          <Btn title='Submit' bg='#bdbdbd' isDisable={isDisabled} />
-          {this.state.showMsg ? <span> added to Address Book</span> : null}
-        </div>
-      </form>
-    );
-  }
+	render() {
+		let isDisabled =
+			this.props.item.username === '' ||
+			this.props.item.email === '' ||
+			this.props.item.phone === '';
+		return (
+			<form style={styles.form} onSubmit={this.handleSubmitForm}>
+				<h2 style={{ textAlign: 'center' }}>
+					{this.props.editMode ? 'Update' : 'New Contact'}
+				</h2>
+				<InputBox
+					nameProp="username"
+					placeholder="Username"
+					regex={/^[a-z0-9]{2,}$/}
+					errorText="username cannot include space"
+					value={this.props.item.username}
+					onChangeProps={this.onChangeHandle}
+				/>
+				<InputBox
+					nameProp="email"
+					placeholder="Email"
+					regex={/^[a-z0-9]{3,}@[2-z]{2,}\.[a-z]{2,4}$/i}
+					errorText="invalid E-mail format"
+					value={this.props.item.email}
+					onChangeProps={this.onChangeHandle}
+				/>
+				<InputBox
+					nameProp="phone"
+					placeholder="Phone No."
+					regex={/^07[0-9]{9}$/}
+					errorText="invalid Mobile Number format"
+					value={this.props.item.phone}
+					onChangeProps={this.onChangeHandle}
+				/>
+				<div style={styleBtn}>
+					<Btn
+						title={this.props.editMode ? 'Update' : 'Submit'}
+						bg="#bdbdbd"
+						isDisable={isDisabled}
+					/>
+					{this.state.showMsg ? <span> added to Address Book</span> : null}
+				</div>
+			</form>
+		);
+	}
 }
 const styleBtn = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-around',
-  alignItems: 'center',
-  marginTop: 10,
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'space-around',
+	alignItems: 'center',
+	marginTop: 10,
 };
